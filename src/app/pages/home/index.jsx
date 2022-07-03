9091dd61aa029f4196fddcb26ec017664d584a25
kknh <kimkunho@live.com>
2022-07-03 18:26:59 +0200

    remove err boundary

--- src/app/pages/home/index.jsx
+++ src/app/pages/home/index.jsx
@@ -1,21 +1,11 @@
 import Posts from '../../components/posts'
-import { useNavigate } from 'react-router-dom'
-import { ErrorBoundary } from 'react-error-boundary'
-import ErrorFallback from '../../utils/error-fallback'
+
 const Home = () => {
 	console.log('Home rendered')
-	const navigate = useNavigate()
 
 	return (
 		<>
-			<ErrorBoundary
-				FallbackComponent={ErrorFallback}
-				onReset={() => {
-					navigate(-1)
-				}}
-			>
-				<Posts />
-			</ErrorBoundary>
+			<Posts />
 		</>
 	)
 }
